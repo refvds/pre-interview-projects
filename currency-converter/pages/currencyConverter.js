caefc767d3386802aab629bbdccb439ac60c5caa
refvds <refvdsk@gmail.com>
2024-02-05 13:07:52 +0300

    feat: add date-time component

--- currency-converter/pages/currencyConverter.js
+++ currency-converter/pages/currencyConverter.js
@@ -1,5 +1,34 @@
+import { addZero } from '../utils/addZero';
+
+const dateTime = () => {
+  const date = new Date();
+
+  let day = addZero(date.getDate());
+  let month = addZero(date.getMonth() + 1);
+
+  let currentDay = `${day}.${month}.${date.getFullYear()} `;
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let formattedHours = hours < 10 ? `0${hours}` : hours;
+  let formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  return `<span>Today, ${currentDay} ${formattedHours}:${formattedMinutes}</span>`;
+};
+
 export const CurrencyConverterPage = () => {
   const page = document.createElement('div');
-  page.innerHTML = `<p>Page 1</p>`;
+  const dateElement = document.createElement('p');
+
+  const updateTime = () => {
+    dateElement.innerHTML = dateTime();
+  };
+
+  updateTime();
+  setInterval(updateTime, 60 * 1000);
+
+  page.innerHTML = `<p>Currency Converter</p>`;
+  page.appendChild(dateElement);
   return page;
 };
