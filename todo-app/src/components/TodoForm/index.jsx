60669ea166c3aa43e75d9dca49f1619c1d351295
refvds <refvdsk@gmail.com>
2024-02-28 19:56:38 +0300

    feat: add check of string's length

--- todo-app/src/components/TodoForm/index.jsx
+++ todo-app/src/components/TodoForm/index.jsx
@@ -5,18 +5,22 @@ export const TodoForm = ({ setTodos, todos }) => {
 
   const inputHandler = (e) => setTodo(e.target.value);
 
-  const keyDownHandler = (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (todo.length <= 14 && todo.length !== 0) {
       setTodos((prev) => [...prev, { id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 1, text: todo }]);
       setTodo('');
     }
   };
 
+  const keyDownHandler = (e) => {
+    if (e.key === 'Enter') {
+      addTodo(e);
+    }
+  };
+
   const clickHandlder = (e) => {
-    e.preventDefault();
-    setTodos((prev) => [...prev, { id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 1, text: todo }]);
-    setTodo('');
+    addTodo(e);
   };
 
   return (
